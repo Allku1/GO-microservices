3da9000b0ef3311216446044f4ff335801b9cdbd
Алексей <allkui0304@gmail.com>
2025-01-28 13:28:44 +0300

    add new branch for every new episode and add logic to hadle post request also test it

--- handlers/products.go
+++ handlers/products.go
@@ -21,16 +21,34 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//update
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
 
 	//catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
-func (p *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(prod)
 }
